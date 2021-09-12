0fe22e37b73bb3bd0fba8ca3a16434986cc83ebd
Derek Zhou <dzhou84@gatech.edu>
2021-09-12 10:46:33 -0400

    Initial Commit

---
+++ BirthdayApp/BirthdayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayAppApp.swift
+//  BirthdayApp
+//
+//  Created by Derek Zhou on 9/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
